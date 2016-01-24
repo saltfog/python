5d9f3dd88426fa5c50cfdc1904536572326f398b
Casey Jenkins <saltfog@gmail.com>
2016-01-23 21:23:10 -0700

    Playing with lists

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print(5+6)
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 
-print(type(str(45)))
+# Sum of kitchen and bedroom area: eat_sleep_area
+print(areas[3])
+print(areas[-1])
+
+house = areas[-1][1]
+#eat_sleep_area = areas[3] + areas[-2]
+
+# Print the variable eat_sleep_area
+#print(eat_sleep_area)
